136110c70eef6831da5d3a83a44a343fd7ae70f6
mateus camanini <camanini28@gmail.com>
2025-05-15 09:54:10 -0300

    Adicionar arquivos de projeto.

---
+++ MauiaApHotel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiaApHotel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiaApHotel/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiaApHotel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
