9508622c8bf6264b5328b1f690313a2c8ebafe2b
mateus camanini <camanini28@gmail.com>
2025-05-23 17:21:57 -0300

    alteraçoes

--- MauiaApHotel/Views/ContratacaoDaHospedagem.xaml.cs
+++ MauiaApHotel/Views/ContratacaoDaHospedagem.xaml.cs
@@ -6,4 +6,14 @@ public partial class ContratacaoDaHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		//App.Current.MainPage = new HospedagemContratada();//vai para informações do projeto, aterar
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        App.Current.MainPage = new HospedagemContratada();
+    }
 }
